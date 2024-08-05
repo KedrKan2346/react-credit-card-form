d88d5c65c53d89129ec856cce59e1d2160aed248
kedr-kan <157804914+KedrKan2346@users.noreply.github.com>
2024-08-05 18:34:02 -0400

    Fix unocss issue in dev mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// NOTE: "uno.css" does not exist but it fixes [unocss] Entry module not found. Did you add `import 'uno.css'` in your main entry?
+// error.
+import 'uno.css';
 import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
